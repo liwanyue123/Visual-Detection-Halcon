b46acc0bbe49254c79bb009761569af697f9cf76
Wanyue Li <995749329@qq.com>
2023-02-25 14:57:10 +0800

    new

---
+++ wheelDetection/Serializable/SaveAllData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace wheelDetection
+{
+    public class SaveAllData
+    {
+        public static void readRectangleData()
+        {
+
+        }
+        public static void readMeasureData()
+        {
+
+        }
+        public static void readComData()
+        {
+
+        }
+    }
+}
---
+++ wheelDetection/Serializable/SaveDataTool.cs
@@ -0,0 +1,47 @@
+﻿using System;
+ 
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+namespace wheelDetection
+{
+    class SaveDataTool//进行序列号和反序列化
+    {
+        public static void Save2File<T>(T obj,string FileName)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(FileName, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(fs, obj);
+                fs.Flush();
+            }
+            catch  (Exception ex)
+            {
+
+            }
+            finally
+            {
+                if (fs != null)
+                    fs.Close();
+            }
+        }
+
+        public static T FormBetyFile<T>(string path)
+        {
+            try
+            {
+                using (FileStream stream = new FileStream(path, FileMode.Open))
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    return (T)formatter.Deserialize(stream);
+                }
+            }
+            catch (Exception ex)
+            {
+                return default(T);
+            }
+        }
+      
+    }
+}
